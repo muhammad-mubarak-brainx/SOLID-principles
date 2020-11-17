4a4274bdb7a7dd9463d07a36752d972f7a095a9f
Muhammad Mubarak <muhammad.mubarak@brainxtech.com>
2020-11-17 16:02:10 +0500

    added interface_seggretaion_principle

---
+++ interface_segregation_principle.rb
@@ -0,0 +1,37 @@
+class VegetarianFood
+	def eat_potatoe
+	end
+	
+	def eat_carrot
+	end
+	
+end
+
+class NonVegetarianFood
+	def eat_chicken
+	end
+	
+	def eat_beef
+	end
+	
+end
+
+class ChineeseFood
+	def eat_chawmin
+	end
+	
+	def eat_dumplings
+	end
+end
+
+
+class Customer
+	def eat_food
+	end
+end
+
+class Waiter
+	def serve_food
+	end
+end
+
