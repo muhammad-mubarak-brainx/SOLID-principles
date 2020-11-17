d1a76772eaf255af9caab09a80fa209e68c3d833
Muhammad Mubarak <muhammad.mubarak@brainxtech.com>
2020-11-17 10:45:31 +0500

    checking for credentials' cache

--- OCP.rb
+++ OCP.rb
@@ -59,7 +59,7 @@ end
 
 class DateDetailsFormatter
   def format(string)
-    "Log was creates at #{Time.now}, please check details #{string}"
+    "Log was created at #{Time.now}, please check details #{string}"
   end
 end
 
