e6d52f8262f0952d24bbbbf4443386236b9a299f
Muhammad Mubarak <muhammad.mubarak@brainxtech.com>
2020-11-17 15:53:37 +0500

    liskov_principle

---
+++ liskov_principle.rb
@@ -0,0 +1,17 @@
+class UserStatistics
+	def posts
+	# logic here 
+	end
+end
+
+class AdminStatistics < UserStatistics
+	def posts
+		#logic here
+	end
+	
+	def formated_posts
+		#logic here
+	end
+end
+
+
