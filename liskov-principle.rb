b9c24434aadc883c041283fcdfefce0c425de380
Muhammad Mubarak <muhammad.mubarak@brainxtech.com>
2020-11-17 11:35:26 +0500

    created LSP,ISP,DIP

---
+++ liskov-principle.rb
@@ -0,0 +1,20 @@
+class UserStatistic
+  def initialize(user)
+    @user = user
+  end
+
+  def posts
+    @user.blog.posts
+  end
+end
+
+class AdminStatistic < UserStatistic
+  def posts
+    user_posts = super
+    user_posts.select { |post| post.popular? }
+  end
+
+  def formatted_posts
+    posts.map { |post| "title: #{post.title} author: #{post.author}" }.join("\n")
+  end
+end
